76580227ee1b210934b0ebba70c0f3dbb4e1c281
Louise Reid <l_reid@hotmail.co.uk>
2019-03-26 09:32:53 +0000

    receiving data

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,28 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main(List<String> args) {
+void main(List<String> args) async{
+  Map _data = await getData();
+  List _features =_data["features"];
+
+  print(_features);
   
   runApp(MaterialApp(
     home:Scaffold(
       appBar:AppBar(
         title: Text('Quakes'),
         backgroundColor: Colors.deepOrange[700],
-      )
+      ),
+      body: ListView(
+
+      ),
     )
   ));
+}
+
+Future<Map>getData() async{
+String url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+http.Response response = await http.get(url);
+
+return json.decode(response.body);
 }
